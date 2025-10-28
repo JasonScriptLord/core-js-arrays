a4e1b626bd0110fa45dde21d195e4c4774bdb06f
Denis Nasonov <brain0991@gmail.com>
2025-10-28 22:05:18 +0300

    feat: fix results

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -98,8 +98,11 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  const falsy = [0, false, '', NaN, undefined, -0, 0n];
+  return arr.filter((elem) => {
+    return !falsy.includes(elem);
+  });
 }
 
 /**
@@ -112,8 +115,10 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((elem) => {
+    return elem.length;
+  });
 }
 
 /**
@@ -130,8 +135,16 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const result = (
+    arr.reduce((acc, elem) => {
+      return acc + elem;
+    }) / arr.length
+  ).toFixed(2);
+  return result % 1 !== 0 ? Number(result) : Math.trunc(result);
 }
 
 /**
